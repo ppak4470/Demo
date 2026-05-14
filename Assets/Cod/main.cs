ca1d97bd86673cfba3c7d39e4e91d01e412af1b2
ppak4470 <150328116+ppak4470@users.noreply.github.com>
2026-05-14 14:45:01 +0900

    초기작업2
    메인화면 strat 씬 추가

--- Assets/Cod/main.cs
+++ Assets/Cod/main.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
-public class main : MonoBehaviour
+public class SceneStarter : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame()
     {
-        
+        SceneManager.LoadScene("Start");
+        Debug.Log("Strat로 이동");
     }
-
-    // Update is called once per frame
-    void Update()
+    public void ExitGame()
     {
-        
+    Debug.Log("게임 종료");
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false; // 에디터 전용
+        #else
+                Application.Quit(); // 빌드 전용
+        #endif
     }
 }
